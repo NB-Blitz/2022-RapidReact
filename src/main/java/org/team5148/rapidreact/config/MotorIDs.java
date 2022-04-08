bd58c21d44b796ad7c59549a48c5c655dad0bda3
DigiWorm <aus343@gmail.com>
2022-04-08 04:57:56 -0500

    Minimize Files

--- src/main/java/org/team5148/rapidreact/config/MotorIDs.java
+++ src/main/java/org/team5148/rapidreact/config/MotorIDs.java
@@ -6,12 +6,4 @@ public class MotorIDs {
     public static final int BACK_LEFT = 2;
     public static final int FRONT_RIGHT = 3;
     public static final int BACK_RIGHT = 4;
-    public static final int INTAKE = 5;
-    public static final int FEED = 6; // Talon
-    public static final int LEFT_STORAGE = 7; // Talon
-    public static final int RIGHT_STORAGE = 8; // Talon
-    public static final int LAUNCHER_BOTTOM = 9;
-    public static final int LAUNCHER_TOP = 10;
-    public static final int LEFT_CLIMBER = 11;
-    public static final int RIGHT_CLIMBER = 12;
 }
