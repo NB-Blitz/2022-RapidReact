27024872749d7e0a55c4cc32bf25f9afe18cd9e1
DigiWorm <aus343@gmail.com>
2022-04-09 05:03:50 -0500

    Added Odometry Test

--- src/main/java/org/team5148/rapidreact/NTManager.java
+++ src/main/java/org/team5148/rapidreact/NTManager.java
@@ -1,8 +1,10 @@
 package org.team5148.rapidreact;
 
 import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.shuffleboard.ComplexWidget;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 
 /**
  * Network Tables Singleton
@@ -12,6 +14,8 @@ public class NTManager {
 
     // Autonomous
     public ShuffleboardTab autoTab = Shuffleboard.getTab("Autonomous");
+    public Field2d autoField2d = new Field2d();
+    public ComplexWidget autoField2dWidget = autoTab.add("Field", autoField2d);
     public NetworkTableEntry autoAbort = autoTab.add("Is Aborted", false).getEntry();
 
     /**
--- src/main/java/org/team5148/rapidreact/Robot.java
+++ src/main/java/org/team5148/rapidreact/Robot.java
@@ -1,9 +1,18 @@
 package org.team5148.rapidreact;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.MecanumDriveKinematics;
+import edu.wpi.first.math.kinematics.MecanumDriveOdometry;
+import edu.wpi.first.math.kinematics.MecanumDriveWheelSpeeds;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.I2C.Port;
 
+import com.kauailabs.navx.frc.AHRS;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import org.team5148.rapidreact.config.MotorIDs;
@@ -15,73 +24,72 @@ public class Robot extends TimedRobot {
 	private final double RAMP = 0.3;
 
 	// Controllers
-	private XboxController driveController = new XboxController(0);
+	private XboxController m_driveController = new XboxController(0);
 
 	// Motors
-	private CANSparkMax backLeft = new CANSparkMax(MotorIDs.BACK_LEFT, MotorType.kBrushless);
-	private CANSparkMax backRight = new CANSparkMax(MotorIDs.BACK_RIGHT, MotorType.kBrushless);
-	private CANSparkMax frontLeft = new CANSparkMax(MotorIDs.FRONT_LEFT, MotorType.kBrushless);
-	private CANSparkMax frontRight = new CANSparkMax(MotorIDs.FRONT_RIGHT, MotorType.kBrushless);
+	private CANSparkMax m_frontLeft = new CANSparkMax(MotorIDs.FRONT_LEFT, MotorType.kBrushless);
+	private CANSparkMax m_frontRight = new CANSparkMax(MotorIDs.FRONT_RIGHT, MotorType.kBrushless);
+	private CANSparkMax m_backLeft = new CANSparkMax(MotorIDs.BACK_LEFT, MotorType.kBrushless);
+	private CANSparkMax m_backRight = new CANSparkMax(MotorIDs.BACK_RIGHT, MotorType.kBrushless);
+
+	// Encoders
+	private RelativeEncoder m_frontLeftEncoder = m_frontLeft.getEncoder();
+	private RelativeEncoder m_frontRightEncoder = m_frontRight.getEncoder();
+	private RelativeEncoder m_backLeftEncoder = m_backLeft.getEncoder();
+	private RelativeEncoder m_backRightEncoder = m_backRight.getEncoder();
+
+	// Kinematics
+	private Translation2d m_frontLeftLocation = new Translation2d(0.381, 0.381);
+	private Translation2d m_frontRightLocation = new Translation2d(0.381, -0.381);
+	private Translation2d m_backLeftLocation = new Translation2d(-0.381, 0.381);
+	private Translation2d m_backRightLocation = new Translation2d(-0.381, -0.381);
+	private MecanumDriveKinematics m_kinematics = new MecanumDriveKinematics(
+		m_frontLeftLocation,
+		m_frontRightLocation,
+		m_backLeftLocation,
+		m_backRightLocation);
+	private MecanumDriveOdometry m_odometry = new MecanumDriveOdometry(m_kinematics, Rotation2d.fromDegrees(0));
 
 	// Systems
-	private NTManager nt = NTManager.getInstance();
+	private NTManager m_nt = NTManager.getInstance();
+	private AHRS m_navX = new AHRS(Port.kMXP);
 
-	/*
-  _____       _           _   
- |  __ \     | |         | |  
- | |__) |___ | |__   ___ | |_ 
- |  _  // _ \| '_ \ / _ \| __|
- | | \ \ (_) | |_) | (_) | |_ 
- |_|  \_\___/|_.__/ \___/ \__|
-                              
-	*/
+	public void reset() {
+		m_backLeft.setOpenLoopRampRate(RAMP);
+		m_backRight.setOpenLoopRampRate(RAMP);
+		m_frontLeft.setOpenLoopRampRate(RAMP);
+		m_frontRight.setOpenLoopRampRate(RAMP);
+		m_frontLeft.setInverted(true);
+    	m_backLeft.setInverted(true);
+		m_navX.reset();
+		m_odometry.resetPosition(new Pose2d(), new Rotation2d());
+	}
+
+	public MecanumDriveWheelSpeeds getWheelSpeeds() {
+		return new MecanumDriveWheelSpeeds(
+			m_frontLeftEncoder.getVelocity(),
+			m_frontRightEncoder.getVelocity(),
+			m_backLeftEncoder.getVelocity(),
+			m_backRightEncoder.getVelocity());
+	}
+	
 
 	@Override
-	public void robotInit() {
-		backLeft.setOpenLoopRampRate(RAMP);
-		backRight.setOpenLoopRampRate(RAMP);
-		frontLeft.setOpenLoopRampRate(RAMP);
-		frontRight.setOpenLoopRampRate(RAMP);
+	public void teleopInit() {
+		reset();
 	}
 
 	@Override
-	public void robotPeriodic() {}
-
-	/*
-                _                                              
-     /\        | |                                             
-    /  \  _   _| |_ ___  _ __   ___  _ __ ___   ___  _   _ ___ 
-   / /\ \| | | | __/ _ \| '_ \ / _ \| '_ ` _ \ / _ \| | | / __|
-  / ____ \ |_| | || (_) | | | | (_) | | | | | | (_) | |_| \__ \
- /_/    \_\__,_|\__\___/|_| |_|\___/|_| |_| |_|\___/ \__,_|___/
-                                                                                                                    
-	 */
-	@Override
-	public void autonomousInit() {}
-
-	@Override
-	public void autonomousPeriodic() {}
-
-	/*
-  _______   _                                 _           _ 
- |__   __| | |                               | |         | |
-    | | ___| | ___  ___  _ __   ___ _ __ __ _| |_ ___  __| |
-    | |/ _ \ |/ _ \/ _ \| '_ \ / _ \ '__/ _` | __/ _ \/ _` |
-    | |  __/ |  __/ (_) | |_) |  __/ | | (_| | ||  __/ (_| |
-    |_|\___|_|\___|\___/| .__/ \___|_|  \__,_|\__\___|\__,_|
-                        | |                                 
-                        |_|                                 
-	 */
-	@Override
-	public void teleopInit() {}
-
-	@Override
 	public void teleopPeriodic() {
-		double xInput = driveController.getLeftX();
-		double yInput = -driveController.getLeftY();
-		double zInput = -driveController.getRightX();
+		// Odometry
+		Pose2d robotPose2d = m_odometry.update(Rotation2d.fromDegrees(m_navX.getAngle()), getWheelSpeeds());
+		m_nt.autoField2d.setRobotPose(robotPose2d);
 
-		// Deadband
+		// Control
+		double xInput = m_driveController.getLeftX();
+		double yInput = -m_driveController.getLeftY();
+		double zInput = -m_driveController.getRightX();
+
 		if (Math.abs(xInput) < DEADBAND)
 			xInput = 0;
 		if (Math.abs(yInput) < DEADBAND)
@@ -89,9 +97,9 @@ public class Robot extends TimedRobot {
 		if (Math.abs(zInput) < DEADBAND)
 			zInput = 0;
 		
-		backLeft.set(-xInput + yInput - zInput);
-		backRight.set(xInput + yInput + zInput);
-		frontLeft.set(xInput + yInput - zInput);
-		frontRight.set(-xInput + yInput + zInput);		
+		m_backLeft.set(-xInput + yInput - zInput);
+		m_backRight.set(xInput + yInput + zInput);
+		m_frontLeft.set(xInput + yInput - zInput);
+		m_frontRight.set(-xInput + yInput + zInput);		
 	}
 }
