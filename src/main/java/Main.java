6c5b3e32d359a33f6980599ae0d8aee8a8964985
David Schulz <deschulz22@gmail.com>
2023-02-18 10:38:01 -0600

    Add vision code from 2022-RapidReact-Vision repo

---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public final class Main {
+    public static void main(String... args) {
+        CamManager camera = new CamManager();
+    }
+}
+// yes
--- src/main/java/org/team5148/rapidreact/config/LauncherTarget.java
+++
@@ -1,8 +0,0 @@
-package org.team5148.rapidreact.config;
-
-public enum LauncherTarget {
-    Launchpad, // 3200 RPM
-    FieldWall, // 3000 RPM
-    Tarmac, // 2900 RPM
-    LowGoal // 1500 RPM
-}
